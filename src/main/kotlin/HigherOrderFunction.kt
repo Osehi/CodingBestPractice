e510f0d630abb0913d5c2b50e320e4d8fac7fda3
Osehi <osehiaseehilen@gmail.com>
2025-01-06 17:03:43 +0100

    add examples using Task

--- src/main/kotlin/HigherOrderFunction.kt
+++ src/main/kotlin/HigherOrderFunction.kt
@@ -12,7 +12,29 @@ fun main() {
         Task(4, "Write a report", 2)
     )
 
-    // testing a basic higher-order function
+    // 1. Filter tasks based on priority
+    val highPriorityTasks = filterTasks(tasks) { task -> task.priority > 2 }
+    printTasks("High Priority Tasks:", highPriorityTasks)
+
+    // 2. Transform task descriptions to uppercase
+    val uppercaseDescriptions = transformTasks(tasks) { task -> task.description.toUpperCase() }
+    printTasks("Uppercase Descriptions:", uppercaseDescriptions)
+
+    // 3. Find a task with the highest priority
+    val highestPriorityTask = ffindHighestPriorityTaskk(tasks)
+    println("Task with the highest priority: $highestPriorityTask")
+
+
+    printFormattedTasks(tasks){  task -> "${task.id}: ${task.description} [Priority: ${task.priority}]"}
+
+
+
+
+
+
+
+
+        // testing a basic higher-order function
     higherOrderFunction { a, b -> a + b}
 
     // another example of higherOrder function
@@ -20,8 +42,32 @@ fun main() {
     println(" This is the output: ${output}")
 }
 
-fun filterTask(tasks: List<Task>, predicate: (Task) -> Boolean) : List<Task>{
+fun filterTasks(tasks: List<Task>, predicate: (Task) -> Boolean) : List<Task>{
     return tasks.filter(predicate)
+}
+
+fun transformTasks(tasks: List<Task>, transformL:(Task) -> String): List<String> {
+    return tasks.map(transformL)
+}
+
+// Higher-order function to find the task with the highest priority
+fun ffindHighestPriorityTaskk(tasks: List<Task>) : Task?{
+    return tasks.maxByOrNull { it.priority }
+}
+
+// Higher-order function to print tasks with a custom formatter
+fun printFormattedTasks(tasks: List<Task>, formatter: (Task) -> String) {
+    return tasks.forEach { task ->
+        println(formatter(task))
+    }
+
+}
+
+fun  printTasks(header: String, tasks: List<*>) {
+    println(header)
+    tasks.forEach { println(it)}
+    println()
+
 }
 
 /**
